48a8ee957600567e278189bcd3294324a14d0991
singhshambhavi <singh.shambhavi228@gmail.com>
2020-12-06 23:48:24 +0530

    requirement.txt update

--- rank_generator/rank_generator/urls.py
+++ rank_generator/rank_generator/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include(("rank_app.urls", "rank_app"), namespace="v1")),
+    
 ]
